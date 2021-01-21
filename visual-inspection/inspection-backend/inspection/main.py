0f7f4b4ea9acf021c068815163f4a5494b1b71b5
Kilian Kluge <kilian.kluge@gmail.com>
2021-01-21 09:31:30 +0100

    [inspection] Add endpoints for file upload

--- visual-inspection/inspection-backend/inspection/main.py
+++ visual-inspection/inspection-backend/inspection/main.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI
+from typing import Dict
+from fastapi import FastAPI, UploadFile, Form, File
 
 from .routers import frontend
 
 app = FastAPI()
 app.include_router(frontend.router)
+
+
+@app.post("/predict")
+def predict(file: UploadFile = File(...)):
+    return file.filename
+
+
+@app.post("/explain")
+def explain(file: UploadFile = File(...),
+            method: str = Form(...),
+            settings: Dict = Form(...)):
+    return file.filename
