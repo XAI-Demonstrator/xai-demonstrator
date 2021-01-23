50c9edc5a9763323cdb1cd4685a0343aabc2e541
Kilian Kluge <kilian.kluge@gmail.com>
2021-01-23 11:52:56 +0100

    [inspection] Refactor model.predict and create more versatile tests

--- visual-inspection/inspection-backend/inspection/model/predict.py
+++ visual-inspection/inspection-backend/inspection/model/predict.py
@@ -4,22 +4,20 @@ from typing import IO
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from fastapi import UploadFile
 from pydantic import BaseModel
 
 from .model import model
 
 
+# TODO: Define the content of the Prediction
 class Prediction(BaseModel):
     prediction_id: uuid.UUID
     class_id: int
     class_label: str
 
 
-# https://deeplizard.com/learn/video/OO4HD-1wRN8
-
-
-def load_and_preprocess(image_file: IO[bytes]):
+# cf. https://deeplizard.com/learn/video/OO4HD-1wRN8
+def preprocess(image_file: IO[bytes]):
     img = Image.open(image_file)
     img = img.resize((224, 224), Image.NEAREST)
     img_array = tf.keras.preprocessing.image.img_to_array(img)
@@ -28,14 +26,13 @@ def load_and_preprocess(image_file: IO[bytes]):
     return tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
 
 
-def predict(image_file: UploadFile,
+def predict(image_file: IO[bytes],
             model_: tf.keras.Model = model) -> Prediction:
-    input_img = load_and_preprocess(image_file.file)
+    input_img = preprocess(image_file)
 
     prediction = model_.predict(input_img)
 
     class_id = int(np.argmax(prediction))
-
     class_label = tf.keras.applications.mobilenet_v2.decode_predictions(prediction, top=1)[0][0][1]
 
     return Prediction(prediction_id=uuid.uuid4(),
