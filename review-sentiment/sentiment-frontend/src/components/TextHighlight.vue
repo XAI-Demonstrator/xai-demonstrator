0b429ceffa50f239369fb1ccfd1bb0f2a055fc55
Kilian Kluge <kilian.kluge@gmail.com>
2021-01-23 10:39:52 +0100

    [sentiment] Re-design TextHighlight explanation

--- review-sentiment/sentiment-frontend/src/components/TextHighlight.vue
+++ review-sentiment/sentiment-frontend/src/components/TextHighlight.vue
@@ -1,44 +1,54 @@
 <template>
   <div class="highlight-box">
-    <span v-for="pair in explanation" :key="pair.word + Math.random()"
-          v-bind:style="{'color': calcHSL(pair.score), 'font-weight': calcWeight(pair.score)}">{{ whitespace(pair.word) }}{{ pair.word }}</span>
+    <span v-for="pair in processedExplanation" :key="pair.word + Math.random()"
+          v-bind:style="{'color': calcHSL(pair.score)}">{{ whitespace(pair.word) }}{{ pair.word }}</span>
   </div>
 </template>
 
 <script>
 export default {
   name: "TextHighlight",
-  props: [
-    "explanation"
-  ],
+  props: {
+    explanation: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }, maxScore: {
+      type: Number,
+      default: 1.0
+    }, preScaler: {
+      type: Number,
+      default: 5.0
+    }
+  },
   data() {
     return {
       noSpaceInFrontOf: [",", ".", "!", ":", ";", "?"]
     }
   },
+  computed: {
+    processedExplanation() {
+      return this.rescaleScores(this.explanation)
+    }
+  },
   methods: {
     calcHSL(score) {
-      return 'hsl(' + this.calcHue(score) + ',' + this.calcSat(score) + '%,' + this.calcLightness(score) + '%)'
+      return 'hsl(' + this.calcHue(score) + ',100%,' + this.calcLightness(score) + '%)'
     },
     calcHue(score) {
       if (score <= 0) {
-        return "14"
+        return 0
       } else {
-        return "174"
+        return 174
       }
-    },
-    calcSat(score) {
-      return Math.abs(score) * 100
     },
     calcLightness(score) {
       if (score <= 0) {
-        return "87"
+        return Math.min(this.preScaler * Math.abs(score) * 40, 40)
       } else {
-        return "27"
+        return Math.min(this.preScaler * Math.abs(score) * 27, 27)
       }
-    },
-    calcWeight(score) {
-      return 100 + Math.min(1600 * Math.abs(score), 800)
     },
     whitespace(word) {
       if (this.noSpaceInFrontOf.includes(word)) {
@@ -46,7 +56,17 @@ export default {
       } else {
         return " "
       }
-
+    },
+    rescaleScores(explanation) {
+      const factor = this.getScalingFactor(explanation)
+      return explanation.map(function (pair) {
+        return {word: pair.word, score: pair.score * factor}
+      })
+    },
+    getScalingFactor(explanation) {
+      return Math.max(1.0, this.maxScore / explanation.reduce(
+          (prevMax, pair) =>
+              Math.max(prevMax, Math.abs(pair.score)), -Infinity))
     }
   }
 }
@@ -54,7 +74,8 @@ export default {
 
 <style scoped>
 .highlight-box {
-  background-color: #77A6F7;
-  padding: 5px 10px;
+  background-color: #fff;
+  border: 1px solid #D3E3FC;
+  padding: 5px;
 }
 </style>
