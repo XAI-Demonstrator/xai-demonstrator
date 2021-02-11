b8f1a626db534b4903074d6060ca87117f48ebeb
Kilian Kluge <kilian.kluge@gmail.com>
2021-02-11 17:23:19 -0500

    Complete unit tests for UseCaseHeader

--- common/vue-components/xaidemo-ui/tests/unit/UseCaseHeader.spec.js
+++ common/vue-components/xaidemo-ui/tests/unit/UseCaseHeader.spec.js
@@ -12,21 +12,40 @@ describe('UseCaseHeader.vue', () => {
     expect(wrapper.find('div.header-title').text()).toMatch(title)
   })
 
-  it('omits back button  when props.standalone is true', () => {
+  it('omits back button when props.standalone is true', () => {
     const wrapper = shallowMount(UseCaseHeader, {
       propsData: { standalone: true }
     })
- 
+
     expect(wrapper.findAll('div.header-icon').at(0).element).toBeEmptyDOMElement()
   })
 
-  it('adds  back button  when props.standalone is false', () => {
+  it('adds back button when props.standalone is false', () => {
     const wrapper = shallowMount(UseCaseHeader, {
       propsData: { standalone: false }
     })
-  
-   const a = wrapper.findAll('div.header-icon').at(0).find('a');
-   expect(a.exists()).toBe(true)
-   expect(a.attributes().href).toBe('/')
+
+    const a = wrapper.findAll('div.header-icon').at(0).find('a')
+    expect(a.exists()).toBe(true)
+    expect(a.attributes().href).toBe('/')
   })
+
+  it('appends window.location.search to reload link', () => {
+    const searchString = '?method=standard'
+    global.window = Object.create(window)
+    Object.defineProperty(window, 'location', {
+        value: {
+            href: '',
+            search: searchString
+        }
+    })
+
+    const wrapper = shallowMount(UseCaseHeader)
+
+    const a = wrapper.findAll('div.header-icon').at(1).find('a')
+    expect(a.exists()).toBe(true)
+    expect(a.attributes().href).toBe('./' + searchString)
+
+   })
+
 })
