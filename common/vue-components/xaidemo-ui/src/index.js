1e91da3fd70707fb89236dff29b851dcca92b364
Kilian Kluge <kilian.kluge@gmail.com>
2021-02-13 15:09:19 +0100

    Add spinners and bump xaidemo-ui version to 0.2.0

--- common/vue-components/xaidemo-ui/src/index.js
+++ common/vue-components/xaidemo-ui/src/index.js
@@ -1,5 +1,8 @@
 import UseCaseHeader from "@/components/UseCaseHeader.vue";
 import FloatingInfoButton from "@/components/FloatingInfoButton.vue";
+import MultiBounce from "@/components/MultiBounce.vue";
+import SpinningIndicator from "@/components/SpinningIndicator.vue";
+
 import "@/assets/main.css";
 
-export {UseCaseHeader, FloatingInfoButton}
+export {UseCaseHeader, FloatingInfoButton, MultiBounce, SpinningIndicator}
