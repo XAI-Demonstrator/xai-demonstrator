5da263482ddfd3aef88a8d9ef5260f5f3b065a0b
Kilian Kluge <kilian.kluge@gmail.com>
2021-02-26 14:56:13 +0100

    Clean up vue.config.js

--- common/vue-components/xaidemo-ui/vue.config.js
+++ common/vue-components/xaidemo-ui/vue.config.js
@@ -1,12 +1,9 @@
-const {join} = require('path')
-
 module.exports = {
     chainWebpack: config => {
         config.module
             .rule('vue')
             .use('vue-svg-inline-loader')
-            .loader('vue-svg-inline-loader') // in your project
-            // .loader(join(__dirname, '../../index.js'))
+            .loader('vue-svg-inline-loader')
             .options({ /* ... */})
     }
 }
