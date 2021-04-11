0890e7f2ce7e901021f5b494132f7403823ef4ad
Kilian Kluge <kilian.kluge@gmail.com>
2021-04-11 23:30:29 +0200

    Fully functional basic tracker

--- experiment-tracker/tracker/config.py
+++ experiment-tracker/tracker/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     service_name: str = "experiment-tracker"
     # Database settings
-    db_server: str = "http://localhost"
+    db_server: str = "localhost"
     db_port: int = 5984
     db_user: str = "user"
     db_password: str = "pw"
