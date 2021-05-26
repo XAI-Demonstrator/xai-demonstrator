1e883fa4d0e2e0dce7ce98a1653f3ae57083cb33
aldeeb <nader_deeb@yahoo.com>
2021-05-26 12:39:42 +0200

    Adding new parameters to the explain method
    index_of_label_to_explain:
    positive_only_parameter:

--- visual-inspection/inspection-backend/inspection/explainer/explain.py
+++ visual-inspection/inspection-backend/inspection/explainer/explain.py
@@ -27,7 +27,6 @@ def generate_output_image(raw_image: np.ndarray,
     buffered = io.BytesIO()
     exp_image.save(buffered, format="png")
     encoded_image_string = base64.b64encode(buffered.getvalue())
-
     return bytes("data:image/png;base64,", encoding="utf-8") + encoded_image_string
 
 
@@ -39,6 +38,8 @@ class Explanation(BaseModel):
 @traced
 def explain(image_file: IO[bytes],
             method: str,
+            index_of_label_to_explain: int,
+            positive_only_parameter: bool,
             settings: Union[None, Dict[str, Any]] = None,
             model_: tf.keras.models.Model = model) -> Explanation:
     settings = settings or {}
@@ -49,7 +50,6 @@ def explain(image_file: IO[bytes],
     input_image = Image.open(image_file)
     explainer_input = preprocess(input_image)[0]
 
-    raw_image = EXPLAINERS[method](explainer_input, model_, **settings)
-
+    raw_image = EXPLAINERS[method](explainer_input, model_, index_of_label_to_explain,positive_only_parameter, **settings)
     return Explanation(explanation_id=explanation_id,
                        image=generate_output_image(raw_image, input_image.size))
