5a0c1f2c597e515278ad31bcfb310c9425ca623c
aldeeb <nader_deeb@yahoo.com>
2021-05-26 13:06:00 +0200

    Update server.js

--- visual-inspection/inspection-frontend/src/server.js
+++ visual-inspection/inspection-frontend/src/server.js
@@ -11,7 +11,8 @@ export function makeServer({environment = "development"} = {}) {
             this.post("/predict", () => ({
                 prediction_id: 'abc',
                 class_label: labels[Math.floor(Math.random() * labels.length)],
-                class_id: 4
+                class_id: 4,
+               class_percentage : 1.0
             }),
                 {timing: 2000})
 
