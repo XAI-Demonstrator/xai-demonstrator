dde0c680e81d803afd436d3fc1deacd144990090
Kilian Kluge <kilian.kluge@gmail.com>
2021-11-04 11:46:41 +0100

    Include ribbons (xaidemo-ui v0.4.2)

--- common/vue-components/xaidemo-ui/src/index.js
+++ common/vue-components/xaidemo-ui/src/index.js
@@ -2,7 +2,9 @@ import UseCaseHeader from "@/components/UseCaseHeader.vue";
 import FloatingInfoButton from "@/components/FloatingInfoButton.vue";
 import MultiBounce from "@/components/MultiBounce.vue";
 import SpinningIndicator from "@/components/SpinningIndicator.vue";
+import GitHubRibbon from "@/components/GitHubRibbon";
+import XAIStudioRibbon from "@/components/XAIStudioRibbon";
 
 import "@/assets/main.css";
 
-export {UseCaseHeader, FloatingInfoButton, MultiBounce, SpinningIndicator}
+export {UseCaseHeader, FloatingInfoButton, MultiBounce, SpinningIndicator, GitHubRibbon, XAIStudioRibbon}
