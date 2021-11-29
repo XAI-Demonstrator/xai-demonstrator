b06b886bc4020d9bd9c73b19d19ac19f6f1457fa
Kilian Kluge <kilian.kluge@gmail.com>
2021-11-29 21:27:02 +0100

    Refactor backend

--- guess-the-country/country-backend/country/model/predict.py
+++ guess-the-country/country-backend/country/model/predict.py
@@ -1,15 +1,15 @@
 from io import BytesIO
+
 import cv2
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.applications.imagenet_utils import decode_predictions
+
 
 def load_model():
     model = tf.keras.models.load_model("model_mnv2.model")
     return model
+
 
 def predict_image(image, model):
     CATEGORIES = ["Tel-Aviv", "Berlin"]
@@ -26,10 +26,12 @@ def preprocess(image: Image.Image):
 
     return image
 
+
 def preprocess2(img_array):
     IMG_SIZE = 224  # 50 in txt-based
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize image to match model's expected sizing
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 3) / 224  # return the image with shaping that TF wants.
+
 
 def read_imagefile(file):
     image = Image.open(BytesIO(file))
@@ -38,11 +40,14 @@ def read_imagefile(file):
 
 def read_imagefile2(file):
     nparr = np.fromstring(file, np.uint8)
-    img_array = cv2.cvtColor(cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR), cv2.COLOR_BGR2RGB)   # read in the image, convert to grayscale
+    img_array = cv2.cvtColor(cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR),
+                             cv2.COLOR_BGR2RGB)  # read in the image, convert to grayscale
     return img_array
+
 
 def prepare(filepath):
     IMG_SIZE = 224  # 50 in txt-based
-    img_array = cv2.cvtColor(cv2.imread(filepath, cv2.IMREAD_ANYCOLOR), cv2.COLOR_BGR2RGB)   # read in the image, convert to grayscale
+    img_array = cv2.cvtColor(cv2.imread(filepath, cv2.IMREAD_ANYCOLOR),
+                             cv2.COLOR_BGR2RGB)  # read in the image, convert to grayscale
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize image to match model's expected sizing
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 3) / 224  # return the image with shaping that TF wants.
