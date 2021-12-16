345e1bc05395f5fa13721c3714a5871682a2adfc
Kilian Kluge <kilian.kluge@gmail.com>
2021-12-16 13:38:52 +0100

    Fix tests and refactor instrumentation function

--- common/backend-utils/tests/tracking/test_tracking.py
+++ common/backend-utils/tests/tracking/test_tracking.py
@@ -1,30 +1,36 @@
+import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from xaidemo import tracing
-from xaidemo.tracking.middleware import ExperimentTrackerMiddleware
+from xaidemo import tracing, tracking
+from xaidemo.tracking.config import TrackingSettings
 from xaidemo.tracking.record import record_data, settings
 from yarl import URL
 
 tracing.set_up()
 
-app = FastAPI()
-app.add_middleware(ExperimentTrackerMiddleware)
+
+@pytest.fixture
+def test_client(mocker):
+    mocker.patch.object(tracking, "settings", new=TrackingSettings(experiment=True,
+                                                                   collector_url="tmp"))
+
+    app = FastAPI()
+    tracking.instrument_app(app)
+
+    @app.post("/test")
+    def route():
+        record_data("test_key", {"my": "test_value"})
+        record_data("other_key", {"my": "big_house"})
+
+    tracing.instrument_app(app)
+
+    yield TestClient(app)
 
 
-@app.post("/test")
-def route():
-    record_data("test_key", {"my": "test_value"})
-    record_data("other_key", {"my": "big_house"})
-
-
-tracing.instrument_app(app)
-
-test_client = TestClient(app)
-
-
-def test_that_data_is_sent_to_collector(aiomock, mocker):
+def test_that_data_is_sent_to_collector(aiomock, mocker, test_client):
     mocker.patch.object(settings, "experiment", new=True)
     mocker.patch.object(settings, "collector_url", new="http://collector")
+    assert settings.experiment
     aiomock.post(settings.collector_url + "/record", status=200, repeat=True)
 
     test_client.post("/test")
@@ -33,9 +39,10 @@ def test_that_data_is_sent_to_collector(aiomock, mocker):
     assert len(calls) == 2
 
 
-def test_that_collector_timeout_is_handled_gracefully(aiomock, mocker):
+def test_that_collector_timeout_is_handled_gracefully(aiomock, mocker, test_client):
     mocker.patch.object(settings, "experiment", new=True)
     mocker.patch.object(settings, "collector_url", new="http://collector")
+    assert settings.experiment
     aiomock.post(settings.collector_url + "/record", timeout=True, repeat=True)
 
     test_client.post("/test")
@@ -44,7 +51,7 @@ def test_that_collector_timeout_is_handled_gracefully(aiomock, mocker):
     assert len(calls) == 2
 
 
-def test_that_experiments_can_be_turned_off(aiomock, mocker):
+def test_that_experiments_can_be_turned_off(aiomock, mocker, test_client):
     mocker.patch.object(settings, "experiment", new=False)
     mocker.patch.object(settings, "collector_url", new="http://collector")
     assert not settings.experiment
--- common/backend-utils/xaidemo/tracking/__init__.py
+++ common/backend-utils/xaidemo/tracking/__init__.py
@@ -7,10 +7,10 @@ from .record import record_data
 from ..http_client import set_up as _set_up_http_client
 
 
-__all__ = ["instrument", "record_data"]
+__all__ = ["instrument_app", "record_data"]
 
 
-def instrument(app: FastAPI):
+def instrument_app(app: FastAPI):
     if settings.experiment:
         _set_up_http_client(app)
         app.add_middleware(ExperimentTrackerMiddleware)
