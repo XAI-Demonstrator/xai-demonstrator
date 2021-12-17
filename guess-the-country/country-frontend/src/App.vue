dbbf7ae7527352acc5b6af401580816467678522
ddennyd <dennis.winter96@yahoo.de>
2021-12-17 12:34:50 +0100

    Prepare for coding session
    Delete notification, add github xai ribbon, small other changes

--- guess-the-country/country-frontend/src/App.vue
+++ guess-the-country/country-frontend/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app" class="xd-app">
-    <UseCaseHeader title="XAI Streetview"/>
+    <GitHubRibbon url="https://github.com/xai-demonstrator/xai-demonstrator"/>
+    <XAIStudioRibbon url="https://www.xai-studio.de"/>
+    <UseCaseHeader
+        v-bind:standalone="!Boolean(backendUrl)"
+        v-bind:title="useCaseTitle"/>
     <main>
       <section class="xd-section xd-light">
         {{ msg }}
@@ -37,96 +41,6 @@
           <SpinningIndicator class="indicator" v-bind:visible="waitingForExplanation"/>
 
     </main>
-    <NotificationGroup group="foo">
-      <div class="fixed inset-0 flex items-start justify-end p-6 px-4 py-6 pointer-events-none">
-        <div class="w-full max-w-sm">
-          <Notification
-            v-slot="{ notifications, close }"
-            enter="transform ease-out duration-300 transition"
-            enter-from="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4"
-            enter-to="translate-y-0 opacity-100 sm:translate-x-0"
-            leave="transition ease-in duration-500"
-            leave-from="opacity-100"
-            leave-to="opacity-0"
-            move="transition duration-500"
-            move-delay="delay-300"
-          >
-            <div
-              class="w-full max-w-sm mt-4 overflow-hidden bg-white rounded-lg shadow-lg pointer-events-auto ring-1 ring-black ring-opacity-5"
-              v-for="notification in notifications"
-              :key="notification.id"
-            >
-              <div class="p-4">
-                <div class="flex items-start">
-                  <div class="flex-shrink-0">
-                    <svg class="w-6 h-6 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none"
-                         viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                    </svg>
-                  </div>
-                  <div class="ml-3 w-0 flex-1 pt-0.5">
-                    <p class="font-semibold text-gray-800">{{ notification.title }}</p>
-                    <p class="text-sm font-semibold text-gray-500">{{ notification.text }}</p>
-                  </div>
-                  <div class="flex flex-shrink-0 ml-4">
-                    <button @click="close(notification.id)"
-                            class="inline-flex text-gray-400 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400">
-                      <span class="sr-only">Close</span>
-                      <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                           aria-hidden="true">
-                        <path fill-rule="evenodd"
-                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                              clip-rule="evenodd"/>
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Notification>
-        </div>
-      </div>
-    </NotificationGroup>
-
-    <NotificationGroup group="bottom">
-      <div class="fixed inset-x-0 bottom-0 flex items-start justify-end p-6 px-4 py-6 pointer-events-none">
-        <div class="w-full max-w-sm">
-          <Notification
-            v-slot="{ notifications }"
-            enter="transform ease-out duration-300 transition"
-            enter-from="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4"
-            enter-to="translate-y-0 opacity-100 sm:translate-x-0"
-            leave="transition ease-in duration-500"
-            leave-from="opacity-100"
-            leave-to="opacity-0"
-            move="transition duration-500"
-            move-delay="delay-300"
-          >
-            <div
-              class="flex w-full max-w-sm mx-auto mt-4 overflow-hidden bg-white rounded-lg shadow-md"
-              v-for="notification in notifications"
-              :key="notification.id"
-            >
-              <div class="flex items-center justify-center w-12 bg-red-500">
-                <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z"
-                  ></path>
-                </svg>
-              </div>
-
-              <div class="px-4 py-2 -mx-3">
-                <div class="mx-3">
-                  <span class="font-semibold text-red-500">{{ notification.title }}</span>
-                  <p class="text-sm text-gray-600">{{ notification.text }}</p>
-                </div>
-              </div>
-            </div>
-          </Notification>
-        </div>
-      </div>
-    </NotificationGroup>
     <FloatingInfoButton
       v-bind:info-text="infoText"
       v-bind:info-url="infoUrl"
@@ -135,10 +49,8 @@
 </template>
 
 <script>
-import notify from 'notiwind'
-import NotificationGroup from 'notiwind'
 import axios from 'axios'
-import {UseCaseHeader, FloatingInfoButton, SpinningIndicator} from '@xai-demonstrator/xaidemo-ui'
+import {UseCaseHeader, FloatingInfoButton, SpinningIndicator, XAIStudioRibbon, GitHubRibbon} from '@xai-demonstrator/xaidemo-ui'
 
 export default {
   name: 'App',
@@ -146,16 +58,17 @@ export default {
     UseCaseHeader,
     FloatingInfoButton,
     SpinningIndicator,
-    NotificationGroup,
-    Notification
+    GitHubRibbon,
+    XAIStudioRibbon
   },
   data() {
     return {
+      useCaseTitle: 'Guess the country',
+      backendUrl: process.env.VUE_APP_BACKEND_URL,
       prediction: 'Israel',
       other: 'Germany',
       score_ai: 0,
       score_user: 0,
-      tasks: [],
       msg: '',
       url: '',
       waitingForExplanation: false,
@@ -235,17 +148,17 @@ export default {
 
           document.cookie = 'AI=' + this.score_ai + '; Secure'
           console.log(document.cookie)
-          notify({
+         /* notify({
             group: 'foo',
             title: 'Success',
             text: 'The AI predicted: ' + res.data[0] + ' and it was actually taken in ' + result
-          }, 5000)
+          }, 5000) */
         } else {
-          notify({
+       /*   notify({
             group: 'bottom',
             title: 'Wrong',
             text: 'The AI predicted: ' + res.data[0] + ' and it was actually taken in ' + result
-          }, 5000)
+          }, 5000) */
         }
 
         document.getElementById('radio').style.display = 'none'
@@ -313,18 +226,18 @@ export default {
           .split('=')[1])
         this.score_user = this.score_user + 1
         document.cookie = 'User=' + this.score_user + '; Secure'
-        console.log(document.cookie)
+/*
         notify({
           group: 'foo',
           title: 'Success',
           text: 'The picture was taken in ' + result
-        }, 3000)
+        }, 3000) */
       } else {
-        notify({
+   /*     notify({
           group: 'bottom',
           title: 'Wrong',
           text: 'The picture was taken in ' + result
-        }, 3000)
+        }, 3000) */
       }
     }
 
--- guess-the-country/country-frontend/src/main.js
+++ guess-the-country/country-frontend/src/main.js
@@ -1,8 +1,6 @@
 import {createApp} from 'vue'
-import Notifications from 'notiwind'
 import App from './App.vue'
 import '@xai-demonstrator/xaidemo-ui/lib/xaidemo-ui.css'
 
 const app = createApp(App)
-app.use(Notifications)
 app.mount('#app')
