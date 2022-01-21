90fac93a6022e16faddad487c06bc06a85720769
Matthias271828 <mat.nase@web.de>
2022-01-21 18:14:23 +0100

    Added generate_visual_explanation method

--- visual-inspection/inspection-backend/inspection/explainer/explainers/new_lime_.py
+++ visual-inspection/inspection-backend/inspection/explainer/explainers/new_lime_.py
@@ -114,7 +114,7 @@ def predict_images(images: np.ndarray, model_: tf.keras.models.Model) -> np.ndar
 
 
 def weigh_segments(samples: np.ndarray, predictions: np.ndarray) -> np.ndarray:
-    """Generating list of coefficients to weight segments
+    """Generating list of coefficients to weigh segments
 
 
     Parameters
@@ -137,3 +137,54 @@ def weigh_segments(samples: np.ndarray, predictions: np.ndarray) -> np.ndarray:
 
     model.fit(samples[:-1], p_column)
     return model.coef_
+
+
+def generate_visual_explanation(weighted_segments: np.ndarray, segment_mask: np.ndarray, image: np.ndarray,
+                                threshold: float, volume: int, colour: str) -> np.ndarray:
+    """Generating image with visual explanation
+
+
+    Parameters
+
+    ----------
+    weighted_segment
+    segment_mask
+    image
+    threshold
+    volume
+    colour
+
+    Returns
+    -------
+
+    """
+    # set colour
+    colours = {"green": [119, 184, 53], "blue": [38, 55, 173], "red": [173, 38, 38], "white": [255, 255, 255],
+               "black": [0, 0, 0]}
+    colour = colour.lower()
+    if colour not in colours.keys():
+        colour = "green"
+
+    # normalize coefficients using sigmoid-function: coefficient_i ∈ [0.0, 1.0]
+    n_weighted_segments = data_norm = (weighted_segments - weighted_segments.min()) / (
+            weighted_segments.max() - weighted_segments.min())
+
+    # check if volume is bigger than the amount of segments
+    max_volume = len(np.unique(segment_mask))
+    if volume > max_volume:
+        volume = max_volume
+
+    # differentiate n_weighted_segments with respect to threshold and volume
+    # values less than max(limit, threshold) are set to 0
+    limit = np.sort(np.unique(n_weighted_segments))[-volume]
+    n_d_weighted_segments = np.where(n_weighted_segments >= max(limit, threshold), n_weighted_segments,
+                                     0)
+    # manipulate the original image (quick and dirty)
+    c = np.array(colours[colour])
+    indices = np.argwhere(n_d_weighted_segments != 0)
+    for i, row in enumerate(segment_mask):
+        for j, el in enumerate(row):
+            if el in indices:
+                image[i, j] = round(n_d_weighted_segments[el], 1) * c / 255
+
+    return image
