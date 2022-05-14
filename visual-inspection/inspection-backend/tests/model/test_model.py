be48fb4814717273bf0ec84cc244ed864c241a93
Kilian Kluge <dev@kluge.ai>
2022-05-14 15:22:07 +0200

    Add support for loading HDF5 models

--- visual-inspection/inspection-backend/tests/model/test_model.py
+++ visual-inspection/inspection-backend/tests/model/test_model.py
@@ -1,35 +1,27 @@
 import pytest
 from fastapi import HTTPException
+import tensorflow as tf
 
 from inspection.model import model
 
 
-def test_that_finding_no_models_raises_error(mocker, tmp_path):
+def test_that_finding_no_models_raises_error(tmp_path):
     models_dir = tmp_path / "models"
     models_dir.mkdir()
 
-    p = mocker.patch.object(model, "PATH")
-    p.return_value = tmp_path
-
     with pytest.raises(ValueError):
-        _ = model._load_models()
+        _ = model._load_models(tmp_path)
 
 
-def test_that_broken_model_is_handled_gracefully(mocker, tmp_path):
+def test_that_broken_model_is_handled_gracefully(tmp_path):
     models_dir = tmp_path / "models"
     models_dir.mkdir()
 
     broken_model_dir = models_dir / "some-empty-model-dir"
     broken_model_dir.mkdir()
 
-    p = mocker.patch.object(model, "PATH")
-    p.return_value = tmp_path
-
-    m = mocker.patch.object(model, "tf")
-    m.side_effect = IOError
-
     with pytest.raises(ValueError):
-        _ = model._load_models()
+        _ = model._load_models(tmp_path)
 
 
 def test_that_missing_model_raises_exception(mocker):
@@ -37,3 +29,25 @@ def test_that_missing_model_raises_exception(mocker):
 
     with pytest.raises(HTTPException):
         model.get_model("missing-model")
+
+
+def test_that_we_can_load_savedmodel_models(tmp_path):
+    models_dir = tmp_path / "models"
+    models_dir.mkdir()
+
+    mobilenet = tf.keras.applications.mobilenet_v2.MobileNetV2()
+    mobilenet.save(models_dir / "my_savedmodel_model")
+
+    assert (models_dir / "my_savedmodel_model").exists()
+    assert "my_savedmodel_model" in model._load_models(tmp_path)
+
+
+def test_that_we_can_load_hdf5_models(tmp_path):
+    models_dir = tmp_path / "models"
+    models_dir.mkdir()
+
+    mobilenet = tf.keras.applications.mobilenet_v2.MobileNetV2()
+    mobilenet.save(models_dir / "my_h5_model.h5")
+
+    assert (models_dir / "my_h5_model.h5").exists()
+    assert "my_h5_model" in model._load_models(tmp_path)
