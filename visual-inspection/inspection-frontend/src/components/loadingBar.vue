b064e4638f407f96d7dc5a96880ee0957849c2ec
Matthias271828 <mat.nase@web.de>
2022-05-23 18:59:16 +0200

    Add loading-bar component

---
+++ visual-inspection/inspection-frontend/src/components/loadingBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loadingBar">
+    <MultiBounce  v-if="time < 100"
+                 v-bind:numberOfDots="3"/>
+    </div>
+
+</template>
+
+<script>
+import {MultiBounce} from '@xai-demonstrator/xaidemo-ui'
+export default {
+  components: {
+    MultiBounce
+  },
+  name: "loadingBar",
+  data: () => {
+    return {
+      time: 0,
+    }
+  },
+  created() {
+    var intval = setInterval(() => {
+      if(this.time < 100)
+        this.time += .92;
+      else
+        clearInterval(intval);
+        this.$emit('loading-done', true)
+    }, 10);
+  }
+}
+</script>
+
+
