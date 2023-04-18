7d1b5f619db555f8474643b22e048f531498009f
bergschaf <christian.k@netcom-mail.de>
2023-04-18 13:21:08 +0200

    test parametrize

--- visual-inspection/inspection-backend/tests/explainer/test_explain.py
+++ visual-inspection/inspection-backend/tests/explainer/test_explain.py
@@ -9,30 +9,17 @@ from inspection.explainer import explain
 
 
 @pytest.mark.integration
-def test_that_an_explanation_is_generated_lime(generate_image):
+@pytest.mark.parametrize("method", ["lime", "visualime"])
+def test_that_an_explanation_is_generated(generate_image, method):
     image_size = (200, 200)
     input_img = generate_image(*image_size)
 
     output_img_bytes = explain.explain(input_img,
                                        model_id=settings.default_model,
-                                       method="lime").image
+                                       method=method).image
     img_data = base64.b64decode(output_img_bytes[22:])
     output_img = Image.open(io.BytesIO(img_data))
 
     assert output_img_bytes[:22] == bytes("data:image/png;base64,", encoding='utf-8')
     assert output_img.size == image_size
 
-
-@pytest.mark.integration
-def test_that_an_explanation_is_generated_visualime(generate_image):
-    image_size = (200, 200)
-    input_img = generate_image(*image_size)
-
-    output_img_bytes = explain.explain(input_img,
-                                       model_id=settings.default_model,
-                                       method="visualime").image
-    img_data = base64.b64decode(output_img_bytes[22:])
-    output_img = Image.open(io.BytesIO(img_data))
-
-    assert output_img_bytes[:22] == bytes("data:image/png;base64,", encoding='utf-8')
-    assert output_img.size == image_size
