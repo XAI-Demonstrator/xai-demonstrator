a78f0cf80bd889b258e4e74e8d20325aef44ae6c
Kilian Kluge <dev@kluge.ai>
2023-09-12 21:52:54 +0200

    finalize conversion to vite and vitest

--- guess-the-country/country-frontend/jest.setup.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom'
-
-if (!('WebkitAppearance' in document.documentElement.style)) {
-    document.documentElement.style['WebkitAppearance'] = ''
-}
--- guess-the-country/country-frontend/vite.config.js
+++ guess-the-country/country-frontend/vite.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    test: {
+        environment: 'happy-dom'
+    },
 });
